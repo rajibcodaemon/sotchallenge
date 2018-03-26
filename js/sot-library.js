518b4abfacd83e74b5a791d3335279077d8b66b7
Your Name <rajib.naskar@codaemonsoftwares.com>
2018-03-26 22:21:08 +0530

    Javascript and UI Modification for mobile and desktop view

--- js/sot-library.js
+++ js/sot-library.js
@@ -209,12 +209,12 @@ function apiCallBackAll(data, callurl, method, flag) {
                         window.setTimeout(function() {
                             $("#progressbar").addClass("hidden")
                             resolve(data)
-                        }, 21000);
+                        }, 21500);
                     } else {
                         window.setTimeout(function() {
                             $("#progressbar").addClass("hidden")
                             reject(data)
-                        }, 21000);
+                        }, 21500);
                     }
                 } else {
                     if (data.status == 'success') {
@@ -230,7 +230,7 @@ function apiCallBackAll(data, callurl, method, flag) {
                     window.setTimeout(function() {
                         $("#progressbar").addClass("hidden")
                         reject(error)
-                    }, 21000);
+                    }, 21500);
                 } else {
                     reject(error)
                 }
